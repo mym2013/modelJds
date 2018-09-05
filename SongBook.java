bccf1f174c2fe7b6cff30b66dbffb5b28791a6c9
mym2013 <torreslefierrogmail.com>
2018-09-05 04:26:27 +0000

    adding a new file named SongBook.java

---
+++ SongBook.java
@@ -0,0 +1,16 @@
+// lo primero que hacemos es crear un indicador mostrando donde estamos ubicados (una especia de mapa virtual)
+package com.teamLaunch.model;
+
+import java.util.List;
+
+public class SongBook{
+	private List<Song> mSong; 
+	
+	public SongBook(){
+		mSong = new ArrayList<Song>();
+ 				
+		
+	}
+
+
+}

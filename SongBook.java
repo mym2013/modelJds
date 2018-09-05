1560d780ee80b827b76ff63081f0161d9823ee7b
mym2013 <torreslefierrogmail.com>
2018-09-05 17:05:39 +0000

    adding moifications just more code complete the ticket

--- SongBook.java
+++ SongBook.java
@@ -2,15 +2,30 @@
 package com.teamLaunch.model;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class SongBook{
-	private List<Song> mSong; 
+	private List<Song> mSongs; 
 	
 	public SongBook(){
-		mSong = new ArrayList<Song>();
- 				
-		
+		mSongs = new ArrayList<Song>();
+
 	}
+	
+//	ahora creamos un metodo apra agregar canciones, así cuando llamemos a este metodo se agregará una canción(deberá aceptar como parametro una canción) 
+//	La canción deberá ser ingresada de acuerdo al formato que le dimos a la clase Song
 
-
+	public void addSong(Song cancion){
+		mSongs.add(cancion);
+  }
+	
+//	Ahora creamos un par de metodos para mostrar algunas caracteristica de nuestra clase, por ejemplo el largo de la lista de canciones
+	
+ public int  getSongCount(){
+	return mSongs.size();
+ }
+	
+	
+	
+	
 }
